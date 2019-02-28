984211e0ead88ed16f95694fd156a4021f3782a9
Jeff M <jkdmarks@gmail.com>
2019-02-28 22:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   for (var i=0; i<facts.length; i++) {
-    facts[i] + '!!!'
+    facts[i] = facts[i] + '!!!'
   }
   return facts;
 }
